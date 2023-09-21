19879d9992940b7ff8733eb077bac715a798e6b2
Riddhi Hakani <rhakani@usc.edu>
2023-09-20 22:55:57 -0700

    added gold

--- Assets/Scripts/GoldCollider.cs
+++ Assets/Scripts/GoldCollider.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AsteroidCollider : MonoBehaviour
+public class GoldCollider : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 1.0f;
